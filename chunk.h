d082724a703742ea7d2a0417e37b2a45b21b0bd3
techoverflow360 <nayankumar200222@gmail.com>
2023-10-15 18:20:33 +0530

    added new data types and new binary operations

--- chunk.h
+++ chunk.h
@@ -13,6 +13,13 @@ typedef enum {
     OP_SUBTRACTION,
     OP_MULTIPLY,
     OP_DIVIDE,
+    OP_NIL,
+    OP_TRUE,
+    OP_FALSE,
+    OP_NOT,
+    OP_EQUAL,
+    OP_GREATER,
+    OP_LESS,
 } OpCode;
 
 typedef struct {
