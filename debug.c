cd379d070d0729221dbe8a1baabaa81b4e8bc47f
ranjanmangla1 <ranjanmangla1@gmail.com>
2023-10-18 09:26:53 +0530

    added support for local variables and added lexical scoping
    Signed-off-by: ranjanmangla1 <ranjanmangla1@gmail.com>

--- debug.c
+++ debug.c
@@ -8,6 +8,13 @@ static int simpleInstruction(const char* name,int offset){
     return offset+1;
 }
 
+
+static int byteInstruction(const char* name, Chunk* chunk, int offset) {
+    uint8_t slot = chunk->code[offset+1];
+    printf("%-16s %4d\n", name, slot);
+    return offset + 2;
+}
+
 static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     uint8_t constant = chunk->code[offset+1];
     printf("%-16s %4d ' ", name , constant);
@@ -22,6 +29,7 @@ void disassembleChunk(Chunk* chunk,const char* name){
         offset = disassembleInstruction(chunk,offset);
     }
 }
+
 
 int disassembleInstruction(Chunk* chunk,int offset){
     printf("%04d ",offset);
@@ -42,6 +50,10 @@ int disassembleInstruction(Chunk* chunk,int offset){
             return constantInstruction("OP_CONSTANT", chunk , offset);
         case OP_NIL:
             return simpleInstruction("OP_NIL", offset);
+        case OP_SET_LOCAL:
+            return byteInstruction("OP_SET_LOCAL", chunk,offset);
+        case OP_GET_LOCAL:
+            return byteInstruction("OP_GET_LOCAL", chunk,offset);
         case OP_GET_GLOBAL:
             return constantInstruction("OP_GET_GLOBAL", chunk, offset);
         case OP_DEFINE_GLOBAL:
